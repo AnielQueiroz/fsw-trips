0b0d163b6c6f053497eac64810db71c4eab7c00c
AnielQueiroz <anielqsilva@gmail.com>
2024-06-24 15:43:35 -0300

    feat: add quick search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import TripeSearch from "./components/TripSearch";
+import QuickSearch from "./components/QuickSearch";
 
 export default function Home() {
 
   return (
     <div>
       <TripeSearch />
+      <QuickSearch />
     </div>
   );
 }
