2a06ef9243bcdb484239fd0ce3cdd3a40b80ab1f
Aniel <anielqsilva@gmail.com>
2024-07-19 20:57:04 -0300

    sobe td

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,8 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import React from "react";
-import Image from "next/image";
-import ReactCountryFlag from "react-country-flag";
 import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
 
 const getTripDetails = async (tripId: string) => {
     const trip = await prisma.trip.findUnique({ where: { id: tripId } });    
@@ -20,6 +19,7 @@ const TripDetails = async ({params}: {params: { tripId: string }}) => {
             <TripHeader trip={trip} />
 
             {/* Reservas */}
+            <TripReservation />
         </div>
     );
 };
